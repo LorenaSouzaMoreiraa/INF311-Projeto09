ae47bbb8f25e534edee30aeff2148311719cd45c
Erick Soares <erick.s.souza@ufv.br>
2025-06-28 03:17:53 -0300

    atualizando versão para lançar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.inf311_projeto09"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
